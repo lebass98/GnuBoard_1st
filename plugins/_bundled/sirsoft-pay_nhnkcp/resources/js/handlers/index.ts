29c25376bc7f72d92cbca3892f170ec9780f23f5
HeuJung <heuristing@gmail.com>
2026-07-14 15:09:12 +0900

    공개 버전 출시를 위한 나이스페이먼츠·NHN KCP 결제 플러그인 번들 제거

--- plugins/_bundled/sirsoft-pay_nhnkcp/resources/js/handlers/index.ts
+++
@@ -1,9 +0,0 @@
-import { requestPaymentHandler } from './requestPayment';
-import { setPaymentMethodHandler } from './setPaymentMethod';
-import { copyToClipboardHandler } from './copyToClipboard';
-
-export const handlerMap: Record<string, (...args: unknown[]) => unknown> = {
-    requestPayment: requestPaymentHandler,
-    setPaymentMethod: setPaymentMethodHandler,
-    copyToClipboard: copyToClipboardHandler,
-} as const;
--- plugins/_bundled/sirsoft-pay_nicepayments/resources/js/handlers/index.ts
+++
@@ -1,7 +0,0 @@
-import { requestPaymentHandler } from './requestPayment';
-import { setPaymentMethodHandler } from './setPaymentMethod';
-
-export const handlerMap: Record<string, (...args: unknown[]) => unknown> = {
-    requestPayment: requestPaymentHandler,
-    setPaymentMethod: setPaymentMethodHandler,
-} as const;
